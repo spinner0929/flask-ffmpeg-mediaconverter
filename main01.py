d0dc59efd747e3e63754e010920f19e030dd51b5
spinner0929 <spinner0929@gmail.com>
2021-02-23 03:45:04 +0900

    forme
    for me

--- main01.py
+++ main01.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory, current_app, make_response
 from werkzeug.utils import secure_filename
 import os
-import subprocess as sp
+import subprocess
 
 FFMPEG_BIN = "ffmpeg.exe"
 
@@ -36,7 +36,7 @@ def upload():
 		if file and allowed_file(file.filename):
 
 			# Server-side check to prevent client-side value manipulation
-			format = request.form.get("format_select")
+			format = "wav"
 			if str(format) in ALLOWED_EXTENSIONS:
 				filename = secure_filename(file.filename)
 				file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
@@ -45,9 +45,9 @@ def upload():
 				dfile = '{}.{}'.format(os.path.splitext(filename)[0], str(format)) # Build file name
 				inputF = os.path.join(app.config['UPLOAD_FOLDER'], filename) # Build input path
 				outputF = os.path.join(app.config['DOWNLOAD_FOLDER'], dfile) # Build output path and add file
-				convertCMD = [FFMPEG_BIN, '-y', '-i', inputF, outputF] # Ffmpeg is flexible enough to handle wildstar conversions
+				convertCMD = [FFMPEG_BIN, '-y', '-i', '-ac', '1', '-ar', '44100', '-acodec', 'pcm_s16le', inputF, outputF] # Ffmpeg is flexible enough to handle wildstar conversions
 
-				executeOrder66 = sp.Popen(convertCMD)
+				executeOrder66 = subprocess.Popen(convertCMD)
 
 				try:
 					outs, errs = executeOrder66.communicate(timeout=5) # tell program to wait
